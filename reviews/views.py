91881ed29825d26f23434d632e1db4b5f85ddeac
justinlove0987 <justinlove0987@gmail.com>
2021-09-30 09:39:19 +0800

    137 first commit

--- reviews/views.py
+++ reviews/views.py
@@ -9,10 +9,16 @@ def review(request):
     if request.method == 'POST':
         # request.POST gives us access to the data itself. It will gives us a dictionary where the keys are the names set on the inputs in the form, and the value are the entered values.
         entered_username = request.POST['username']
+        if entered_username == "" and len(entered_username) >= 100:
+            return render(request, "reviews/review.html", {
+                "has_error": True
+            })
         print(entered_username)
         return HttpResponseRedirect("/thank-you")
 
-    return render(request, "reviews/review.html")
+    return render(request, "reviews/review.html", {
+        "has_error": False
+    })
 
 
 # POST request is meant to subit data to the server not to get some page.
