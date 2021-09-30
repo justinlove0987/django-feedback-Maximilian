88136d9d7398e3a414e742f69b178a56faf875d2
justinlove0987 <justinlove0987@gmail.com>
2021-09-30 09:28:51 +0800

    136 first commit

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,21 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
 
 # Create your views here.
 
 def review(request):
+    # method property which tells us the HTTP method which was used for this request.
+    if request.method == 'POST':
+        # request.POST gives us access to the data itself. It will gives us a dictionary where the keys are the names set on the inputs in the form, and the value are the entered values.
+        entered_username = request.POST['username']
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
+
     return render(request, "reviews/review.html")
+
+
+# POST request is meant to subit data to the server not to get some page.
+# instead of rendering a template, we redirect to a dirrent URL with a GET reuqest.
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
