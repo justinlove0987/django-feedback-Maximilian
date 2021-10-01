2e2e14b3480fb035b1056696e25929da58dde9f8
justinlove0987 <justinlove0987@gmail.com>
2021-10-01 10:03:17 +0800

    140 first commit

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    # required validators is defaul.
+    # https://docs.djangoproject.com/en/3.2/ref/forms/fields/
+    user_name = forms.CharField(label="Your Name", max_length=10, error_messages={
+        "required": "Your name must not be empty",
+        "max_length": "Please enter a shorter name!"
+    })
