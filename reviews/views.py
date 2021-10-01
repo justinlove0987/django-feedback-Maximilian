be095f3d786c98bb4b35b7075f12fd9eb86af528
justinlove0987 <justinlove0987@gmail.com>
2021-10-02 07:51:20 +0800

    147 first commit

--- reviews/views.py
+++ reviews/views.py
@@ -11,13 +11,19 @@ def review(request):
     # method property which tells us the HTTP method which was used for this request.
     if request.method == 'POST':
         form = ReviewForm(request.POST)
+        # When form.save(), we will then save the data not as a new entry,- 
+        # - but to update the existing entry in the database.
+        existing_data = Review.objects.get(pk=1)
+        form = ReviewForm(request.Post, instance=existing_data)
         # .is_valid() will validate the inputs
         if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating = form.cleaned_data['rating'])
-            review.save()
+            # if we use modelform we can skip the step, we don't need to create our model instance.
+            # review = Review(
+            #     user_name=form.cleaned_data['user_name'],
+            #     review_text=form.cleaned_data['review_text'],
+            #     rating = form.cleaned_data['rating'])
+            # review.save()
+            form.save()
             return HttpResponseRedirect("/thank-you")
 
     # if we don't make the value into the if block above, we would recreate the form and rerender the template.
