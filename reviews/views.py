90d2a17c880f2b74caa8f98dcc28918391746b6d
justinlove0987 <justinlove0987@gmail.com>
2021-10-04 10:14:46 +0800

    156 convert ReviewListView class to ListView & filter data which the rating is greater than 4

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
 from .models import Review
@@ -37,20 +38,21 @@ class TankYouView(TemplateView):
         return context
 
 
-class ReviewsListView(TemplateView):
-    def get_template_names(self):
-        return ["reviews/review_list.html"]
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    # django will fetch all the data realted to the model -
+    # - and pass it as context to the template.
+    model = Review
+    context_object_name = "reviews"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
-
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gt=4)
+        return data
+    
 
 class SingleReviewView(TemplateView):
-    def get_template_names(self):
-        return ["reviews/single_review.html"]
+    template_name = "reviews/single_review.html"
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
