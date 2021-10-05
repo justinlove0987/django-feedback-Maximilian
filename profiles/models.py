6069881a2b57d326d2f5a1d72ed1cb12a47445ff
justinlove0987 <justinlove0987@gmail.com>
2021-10-05 08:00:41 +0800

    168 use an ImageField in UserProfile class

--- profiles/models.py
+++ profiles/models.py
@@ -3,8 +3,5 @@ from django.db import models
 # Create your models here.
 
 class UserProfile(models.Model):
-    # the file will not be stored in the database.
-    # instead it will take the file and move it somewhere on our harddrive.
-    # just the path will be stored in the database
-    # django will look the data folder on the root level.
-    image = models.FileField(upload_to="images")
+    # django will only accept image.
+    image = models.ImageField(upload_to="images")
