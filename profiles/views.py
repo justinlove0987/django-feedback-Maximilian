70b0095c6f5ea9dee69f05223191309b8af94ddc
justinlove0987 <justinlove0987@gmail.com>
2021-10-05 08:09:54 +0800

    169 convert CreateProfileView class to CreateView

--- profiles/views.py
+++ profiles/views.py
@@ -1,27 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
 
 # Create your views here.
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
-
-    def post(self, request):
-        submmited_forms = ProfileForm(request.POST, request.FILES)
-
-        if submmited_forms.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-
-        return render(request, "profiles/create_profile.html", {
-            "form": submmited_forms
-        })
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
