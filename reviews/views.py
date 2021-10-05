96b5e14902acaefe1bb0130dcf307fa335648f94
justinlove0987 <justinlove0987@gmail.com>
2021-10-05 10:22:26 +0800

    180 stroe primitive data in sessions

--- reviews/views.py
+++ reviews/views.py
@@ -55,11 +55,9 @@ class SingleReviewView(DetailView):
 
 class AddFavoriteView(View):
     def post(self, request):
-        # review_id = request.POST["review_id"]
         review_id = request.POST["reivew_id"]
-        fav_review = Review.objects.get(pk=review_id)
+        # fav_review = Review.objects.get(pk=review_id)
         # django automatically save the data in the database.
-        request.session["favorite_review"] = fav_review
-        # return HttpResponseRedirect("/reviews/" + review_id)
-        # change /reviews/ to reviews/ and it can work
-        return HttpResponseRedirect("reviews/" + review_id)
+        # don't store objects in sessions.
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect("/reviews/" + review_id)
