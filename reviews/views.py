0e9b321a48667baf8e013bd18abb172557b78184
justinlove0987 <justinlove0987@gmail.com>
2021-10-05 10:45:09 +0800

    182 access session data safely

--- reviews/views.py
+++ reviews/views.py
@@ -54,7 +54,8 @@ class SingleReviewView(DetailView):
         context =  super().get_context_data(**kwargs)
         loaded_review = self.object
         request = self.request
-        favorite_id = request.session["favorite_review"]
+        # use get method, it will not throw an error if favorite review doesn't exist yet.
+        favorite_id = request.session.get("favorite_review")
         context["is_favorite"] = favorite_id == str(loaded_review.id)
         return context
     
